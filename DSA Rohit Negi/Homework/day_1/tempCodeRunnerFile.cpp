a80a1773a1f30a3782a42f44de5af3d15ba19d6a
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-20 22:19:16 +0530

    Change folder names in 'DSA Rohit Negi'/Homework/day_1

--- DSA Rohit Negi/Homework/day_1/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-tDigit);
-        // (lastDigit == 11) ? lastDigit = 'B' : to_string(lastDigit);
-        // (lastDigit == 12) ? lastDigit = 'C' : to_string(lastDigit);
-        // (lastDigit == 13) ? lastDigit = 'D' : to_string(lastDigit);
-        // (lastDigit == 14) ? lastDigit = 'E' : to_string(lastDigit);
-        // (lastDigit == 15) ? lastDigit = 'F' : to_string(lastDigit);
