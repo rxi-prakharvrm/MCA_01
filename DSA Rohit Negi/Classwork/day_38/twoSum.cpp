8c36cc3e35c144a727bbe34858cf9033243dcccb
Prakhar Verma <prakharverma1022@gmail.com>
2023-10-26 06:22:31 +0530

    Code for twoSum.cpp in O(n) time is done

--- DSA Rohit Negi/Classwork/day_38/twoSum.cpp
+++ DSA Rohit Negi/Classwork/day_38/twoSum.cpp
@@ -18,22 +18,40 @@ vector<int> twoSum(vector<int> arr, int n, int target) {
 
 
     // nlog(n)
-    int x, start, end, mid;
-    for(int i = 0; i < n-1; i++) {
-        x = target - arr[i];
-        start = i+1;
-        end = n-1;
+    // int x, start, end, mid;
+    // for(int i = 0; i < n-1; i++) {
+    //     x = target - arr[i];
+    //     start = i+1;
+    //     end = n-1;
 
-        while(start <= end) {
-            mid = start + (end - start) / 2;
-            if(x == arr[mid]) {
-                return {arr[i], arr[mid]};
-            }
-            if(x < arr[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+    //     while(start <= end) {
+    //         mid = start + (end - start) / 2;
+    //         if(x == arr[mid]) {
+    //             return {arr[i], arr[mid]};
+    //         }
+    //         if(x < arr[mid]) {
+    //             end = mid - 1;
+    //         } else {
+    //             start = mid + 1;
+    //         }
+    //     }
+    // }
+
+    // O(n)
+    int start = 0;
+    int end = arr.size() - 1;
+
+    while(start < end) {
+        if(arr[start] + arr[end] == target) {
+            ans.push_back(arr[start]);
+            ans.push_back(arr[end]);
+            return ans;
+        }
+
+        if(arr[start] + arr[end] < target) {
+            start++;
+        } else {
+            end--;
         }
     }
 }
