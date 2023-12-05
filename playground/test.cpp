94c85b6d6911ab43730ff293c9505d65078dd364
Prakhar Verma <prakharverma1022@gmail.com>
2023-12-05 22:02:11 +0530

    some test coding is done here

--- playground/test.cpp
+++ playground/test.cpp
@@ -1,12 +1,42 @@
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void sort012(int a[], int n) {
+//     int count = 0;
+//     for(int start = 0; start < n; start++) {
+//         int end = n - 1;
+//         while(a[start] != count) {
+//             if(a[end] == 0 || a[end] == 1) {
+//                 swap(a[start], a[end]);
+//             } else {
+//                 if(end > start) {
+//                     count++;
+//                     end--;
+//                 }
+//                 else break;
+//             }
+//         }
+//     }
+// }
+
+// int main() {
+//     int arr[] = {0, 2, 1, 2, 0, 2, 2, 0, 0, 1, 0, 1, 2};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     sort012(arr, n);
+
+//     for(int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    if(true) {
-        cout << true;
-    } else
-    if(false) {
-        cout << false;
-    }
+int main()
+{
+    cout << max(1,2);
     return 0;
 }
