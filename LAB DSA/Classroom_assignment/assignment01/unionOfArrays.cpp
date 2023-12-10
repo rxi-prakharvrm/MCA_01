1734128356d537a64427da18625f0caf1225461b
Prakhar Verma <prakharverma1022@gmail.com>
2023-12-10 17:23:45 +0530

    Code for union program is changed from set to vector

--- LAB DSA/Classroom_assignment/assignment01/unionOfArrays.cpp
+++ LAB DSA/Classroom_assignment/assignment01/unionOfArrays.cpp
@@ -3,25 +3,45 @@
 using namespace std;
 
 // Returning a union set
-set<int> unionOfArrayElements(vector<int> arr1, vector<int> arr2, set<int> &ans) {
+void unionOfArrayElements(vector<int> arr1, vector<int> arr2, vector<int> &ans) {
 
     // store all the unique elements in ans from arr1
     for(int i = 0; i < arr1.size(); i++) {
-        ans.insert(arr1[i]);
+        for(int j = 0; j < arr2.size(); j++) {
+            ans.push_back(arr1[i]);
+            if(arr1[i] == arr2[j]) {
+                arr2.erase(arr2.begin()+j);
+            }
+        }
     }
 
-    // store all the unique elements in ans from arr2
+    // insert remaining elements of arr2 in ans
     for(int i = 0; i < arr2.size(); i++) {
-        ans.insert(arr2[i]);
+        ans.push_back(arr2[i]);
     }
     
-    return ans;
+    // store -1 at the place of a number occurred more than once
+    for (int i = 0; i < ans.size() - 1; i++) {
+        for(int j = i+1; j < ans.size(); j++) {
+            if(ans[i] == ans[j]) {
+                ans[i] = -1; 
+            }
+        }
+    }
+
+    // remove all the occurrences of -1 and reduce the size of the ans array accordingly
+    for(int i = 0; i < ans.size(); i++) {
+        if(ans[i] == -1) {
+            ans.erase(ans.begin()+i);
+            i--;
+        }
+    }
 }
 
 int main() {
     vector<int> arr1 = {1, 2, 2, 7, 3};
     vector<int> arr2 = {2, 2, 3, 9, 15};
-    set<int> ans;
+    vector<int> ans;
 
     unionOfArrayElements(arr1, arr2, ans);
 
